fb884e58697407f24b044b3249b4a021d12f3fe4
zamuraev <zamuraev.sergey@gmail.com>
2022-03-17 10:03:55 +0200

    Initial commit

---
+++ src/main/java/com/zamuraev/SpringPaginationFilteringSortingApplication.java
@@ -0,0 +1,13 @@
+package com.zamuraev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaginationFilteringSortingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPaginationFilteringSortingApplication.class, args);
+    }
+
+}
